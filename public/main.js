749b886098b77c8b7a9d4cb7491ca45b5c5c44d4
yousuf_castusinfo <yousuf@castusinfo.com>
2021-02-08 17:23:30 +0530

    new turn and stun

--- public/main.js
+++ public/main.js
@@ -9,29 +9,36 @@ var roomNumber, localStream, remoteStream, rtcPeerConnection, isCaller;
 
 var iceServers = {
   iceServers: [
-    { urls: "stun:stun.services.mozilla.com" },
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19305" },
-    { urls: "stun:relay.backups.cz" },
+    // { urls: "stun:stun.services.mozilla.com" },
+    // { urls: "stun:stun.l.google.com:19302" },
+    // { urls: "stun:stun2.l.google.com:19305" },
+    // { urls: "stun:relay.backups.cz" },
+    // {
+    //   urls: "stun:numb.viagenie.ca",
+    //   username: "sultan1640@gmail.com",
+    //   credential: "98376683",
+    // },
+    // {
+    //   urls: "turn:numb.viagenie.ca",
+    //   username: "sultan1640@gmail.com",
+    //   credential: "98376683",
+    // },
+    // {
+    //   url: "turn:relay.backups.cz",
+    //   credential: "webrtc",
+    //   username: "webrtc",
+    // },
+    // {
+    //   url: "turn:relay.backups.cz?transport=tcp",
+    //   credential: "webrtc",
+    //   username: "webrtc",
+    // },
+
+    { urls: "stun:stun.iboring.xyz:5349" },
     {
-      urls: "stun:numb.viagenie.ca",
-      username: "sultan1640@gmail.com",
-      credential: "98376683",
-    },
-    {
-      urls: "turn:numb.viagenie.ca",
-      username: "sultan1640@gmail.com",
-      credential: "98376683",
-    },
-    {
-      url: "turn:relay.backups.cz",
-      credential: "webrtc",
-      username: "webrtc",
-    },
-    {
-      url: "turn:relay.backups.cz?transport=tcp",
-      credential: "webrtc",
-      username: "webrtc",
+      url: "turn:turn.iboring.xyz:5349",
+      username: "yousuf",
+      credential: "yousuf",
     },
   ],
 };
